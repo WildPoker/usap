2d57bc5bbfd1c33d504ef5ea00c123a8bfdaadf2
Denzel Bayani <denzelbayani72@gmail.com>
2020-12-15 20:29:30 -0800

    2nd Commit testing

--- src/components/Dashboard/components/Chat/Chatapp.js
+++ src/components/Dashboard/components/Chat/Chatapp.js
@@ -3,42 +3,36 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import useStyles from "./styles";
 import { db } from "../../../../Firebase";
+import { useAuth } from "../../../../contexts/AuthContext";
 const Chatapp = () => {
   const classes = useStyles();
+  const { currentUser } = useAuth();
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    db.collection("Testing").onSnapshot((snapshot) => {
-      setMessages(snapshot.docs.map((doc) => ({ message: doc.data() })));
-    });
-    console.log(messages);
+    db.collection("Chats")
+      .doc("Rooms")
+      .collection("Room-1")
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => ({ message: doc.data() })));
+      });
+    console.log(currentUser);
   }, []);
 
-  // function readChat() {
-  //   const docRef = db
-  //     .collection("chat")
-  //     .doc("room-1")
-  //     .collection("chats")
-  //     .doc("hORoK317n0XGb8ZJNuLa");
-  //   return docRef
-  //     .get()
-  //     .then(function (doc) {
-  //       if (doc.exists) {
-  //         setChat({ id: doc.id, chat: doc.data() });
-  //       } else {
-  //         // doc.data() will be undefined in this case
-  //         console.log("No such document!");
-  //       }
-  //     })
-  //     .catch(function (error) {
-  //       console.log("Error getting document:", error);
-  //     });
-  // }
   return (
     <Container className={classes.container}>
       <Typography component="div" className={classes.Typography}>
         {messages.map(({ message }) => {
-          <p>{message.message}</p>;
+          return (
+            <div className={classes.messageHold}>
+              <p
+                style={{ wordBreak: "break-all", whiteSpace: "normal" }}
+                className={classes.messageBox}
+              >
+                {message.message}
+              </p>
+            </div>
+          );
         })}
       </Typography>
     </Container>
